8f35f079524df13ebefa2bae5824daf0a6d699a4
Pankaj Kumar <805.bluebell@gmail.com>
2019-03-16 21:14:19 +0530

    Starting first time - directi game

--- index.js
+++ index.js
@@ -3,6 +3,35 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const path = require('path');
+const router = express.Router();
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+//add the router
+app.use('/', router);
+
+
+
+
+
+
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})  
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
