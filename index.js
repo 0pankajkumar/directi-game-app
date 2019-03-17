313607fd9398d8b702a7e2366c321bd59f5b873e
Pankaj Kumar <805.bluebell@gmail.com>
2019-03-17 10:29:58 +0530

    working game now

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000
 const path = require('path');
 const router = express.Router();
 
-router.get('/',function(req,res){
+router.get('/index.html',function(req,res){
   res.sendFile(path.join(__dirname+'/public/index.html'));
   //__dirname : It will resolve to your project folder.
 });
@@ -20,7 +20,7 @@ app.use(express.static('public'))
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/checkI', (req, res) => res.send('Got a GET request'))
 
 app.post('/', function (req, res) {
   res.send('Got a POST request')
