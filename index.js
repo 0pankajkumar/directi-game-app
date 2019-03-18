5e1c1550b72f051b26d558498966fbbdc81fe263
Pankaj Kumar <805.bluebell@gmail.com>
2019-03-18 09:02:02 +0530

    logging people in

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static('public'))
 
 
 
-app.get('/checkI', (req, res) => res.send(this.req.params))
+app.get('/checkI', (req, res) => res.send(req))
 
 app.post('/', function (req, res) {
   res.send('Got a POST request')
