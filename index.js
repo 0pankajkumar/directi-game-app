0ed64b57bc5850301c662a3b02ef7c35c587699d
Pankaj Kumar <805.bluebell@gmail.com>
2019-03-19 17:08:18 +0530

    insert & select options almost implemented

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ var writeBuff = [];
         
         var text1 = "";
         var text2 = "";
-        var text3 = "{";
+        var text3 = "[";
         var flag = true;
 
         //res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -105,7 +105,7 @@ var writeBuff = [];
           
           //res.status(200).send(JSON.stringify(response2));
           
-            text3 = text3.concat("","}");
+            text3 = text3.concat("","]");
             res.send(text3);
             //res.end();
         });
