d7167a6f1a0d1fe401379041500153b1cba11dd5
Pankaj Kumar <805.bluebell@gmail.com>
2019-03-19 11:00:57 +0530

    started with postgres

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const router = express.Router();
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString: process.env.DATABASE_URL || postgres://cgkjafqthibnwj:3cbe5e90f1632d099116b6fbb8cf4891d61f8b3b6fd97d2ee3491c8d637120aa@ec2-50-19-109-120.compute-1.amazonaws.com:5432/d53ttjci9p0oj9,
+  connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
 
